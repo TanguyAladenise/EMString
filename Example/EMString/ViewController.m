2fd2237b0e9db63e4b967e9bd6f35f743e33242b
TanguyAladenise <tanguy.aladenise@gmail.com>
2015-01-24 23:06:36 -0500

    Updating pod

--- Example/EMString/ViewController.m
+++ Example/EMString/ViewController.m
@@ -24,15 +24,6 @@
     [super viewDidLoad];
     
     
-    NSArray *fontFamilies = [UIFont familyNames];
-    
-    for (int i = 0; i < [fontFamilies count]; i++)
-    {
-        NSString *fontFamily = [fontFamilies objectAtIndex:i];
-        NSArray *fontNames = [UIFont fontNamesForFamilyName:[fontFamilies objectAtIndex:i]];
-        NSLog (@"%@: %@", fontFamily, fontNames);
-    }
-    
     self.textView.textContainerInset = UIEdgeInsetsMake(20, 10, 20, 10);
 
 
@@ -45,7 +36,6 @@
 
     self.text = @"<h4>About EMString</h4>\n<p><strong>EMString</strong> stands for <em><strong>E</strong>asy <strong>M</strong>arkup <strong>S</strong>tring</em>. I hesitated to call it SONSAString : Sick Of NSAttributedString...</p>\n<p>Most of the time if you need to display a text with different styling in it, like \"<strong>This text is bold</strong> but then <em>italic.</em>\", you would use an <code>NSAttributedString</code> and its API. Same thing if suddenly your text is <green><strong>GREEN</strong></green> and then <red><strong>RED</strong></red>...</p><p>Personnaly I don't like it! It clusters my classes with a lot of boilerplate code to find range and apply style, etc...</p>\n<p>This is what <strong>EMString</strong> is about. Use the efficient <u>HTML markup</u> system we all know and get an iOS string stylized in one line of code and behave like you would expect it to.</p>\n<h1>h1 header</h1><h2>h2 header</h2><h3>h3 header</h3><stroke>Stroke text</stroke>\n<strong>strong</strong>\n<em>emphasis</em>\n<u>underline</u>\n<s>strikethrough</s>\n<code>and pretty much whatever you think of...</code>";
     
-
     
     EMStylingClass *aStylingClass = [[EMStylingClass alloc] initWithMarkup:@"<code>"];
     aStylingClass.color           = [UIColor colorWithWhite:0.151 alpha:1.000];
@@ -71,11 +61,11 @@
     self.topLabel.attributedText = @"<stroke>EMSString's magic</stroke>".attributedString;
     
     self.textView.attributedText = self.text.attributedString;
-    
 }
 
 
-- (IBAction)switchButtonPressed:(UIButton *)sender {
+- (IBAction)switchButtonPressed:(UIButton *)sender
+{
     if (!sender.selected) {
         self.textView.text = self.text;
         self.textView.font = [UIFont systemFontOfSize:16];
